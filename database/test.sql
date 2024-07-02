5f33746f6b0dcfb1cbb48005bbb80dd70901cff3
Kitt Cymonne Japson <158536264+thereyno143@users.noreply.github.com>
2024-07-02 09:53:22 +0800

    upload database

---
+++ database/test.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v9.62 
+MySQL - 5.6.37-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `ojt_names` */
+
+DROP TABLE IF EXISTS `ojt_names`;
+
+CREATE TABLE `ojt_names` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `transdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `ojt_names` */
+
+insert  into `ojt_names`(`id`,`name`,`transdate`) values (1,'Evander','2024-06-28 16:32:49'),(2,'Janiel','2024-06-28 16:32:53'),(3,'Mary Grace Nalual','2024-06-28 16:58:13'),(4,'Mary Grace Nalual','2024-06-28 16:58:53');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
